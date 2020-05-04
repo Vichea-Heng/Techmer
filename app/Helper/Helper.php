0ff46cf656087fa8fba3d7807b148e8555e8c337
Vichea <mr.vichea.007@gmail.com>
2020-05-04 19:33:59 +0700

    Deleta CheckEmptyArray From Helper

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -2,19 +2,6 @@
 
 use Illuminate\Support\Facades\Storage;
 
-if (!function_exists('check_empty_array')) {
-    function check_empty_array($array, $prop)
-    {
-        try {
-            $array[$prop];
-            if (empty($array[$prop]))
-                return true;
-        } catch (Throwable $th) {
-            return false;
-        }
-    }
-}
-
 if (!function_exists('check_file_exist')) {
     function check_file_exist($path, $fileName, $ext)
     {
