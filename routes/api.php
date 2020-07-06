028da7f82c7ae07709f97693adca522a6d268e1a
Vichea <mr.vichea.007@gmail.com>
2020-07-06 11:47:47 +0700

    a

--- routes/api.php
+++ routes/api.php
@@ -78,7 +78,7 @@ Route::group(["prefix" => "/v1"], function () {
                 // Route::group(["middleware" => CheckSuperAdmin::class], function () {
                 apiSoftDelete("product", "ProductController");
                 // });
-                Route::apiResource('/product', 'ProductController');
+                Route::apiResource('/product', 'ProductController', ["except", ["index", "show"]]);
 
                 Route::get("/product-option/file/{product-option}", 'ProductOptionController@getFile');
                 // Route::group(["middleware" => CheckSuperAdmin::class], function () {
@@ -133,6 +133,7 @@ Route::group(["prefix" => "/v1"], function () {
         });
     });
     Route::apiResource('/product-category', 'Api\\v1\\Products\\ProductCategoryController', ["only", "index"]);
+    Route::apiResource('/product', 'Api\\v1\\Products\\ProductController', ["only", ["index", "show"]]);
 
     Route::group(["namespace" => "Api\\v1\\Addresses\\"], function () {
         // Route::group(["middleware" => CheckSuperAdmin::class], function () {
