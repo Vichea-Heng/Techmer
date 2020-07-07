e5b4feefd331c2b90d4c9415508159b1834a371e
Vichea <mr.vichea.007@gmail.com>
2020-07-07 12:05:02 +0700

    a

--- app/Http/Controllers/Api/v1/Products/ProductController.php
+++ app/Http/Controllers/Api/v1/Products/ProductController.php
@@ -290,6 +290,9 @@ class ProductController extends Controller
             $this->searchAlgorithm($word, $array);
         }
 
+        $tempArr = array_unique(array_column($array, 'id'));
+        $array = array_intersect_key($array, $tempArr);
+
         return dataResponse($array);
     }
 }
