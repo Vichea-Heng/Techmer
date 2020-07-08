b54382ba1b917413ac34054643f916b52d15e76c
Vichea <mr.vichea.007@gmail.com>
2020-07-08 14:06:53 +0700

    a

--- app/Http/Resources/Products/ProductBrandResource.php
+++ app/Http/Resources/Products/ProductBrandResource.php
@@ -12,6 +12,7 @@ class ProductBrandResource extends JsonResource
             "id" => $this->id,
             "brand" => $this->brand,
             "from_country" => $this->country->country,
+            "from_country_id" => $this->from_country,
             "posted_by" => $this->user->identity->full_name,
             'deleted_at' => $this->when(!empty($this->deleted_at), $this->deleted_at),
         ];
