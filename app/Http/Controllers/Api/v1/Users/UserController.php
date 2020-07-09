39102ab27219381c685cf13b4cea2b56b6b6c5b0
Vichea <mr.vichea.007@gmail.com>
2020-07-09 16:08:01 +0700

    a

--- app/Http/Controllers/Api/v1/Users/UserController.php
+++ app/Http/Controllers/Api/v1/Users/UserController.php
@@ -57,7 +57,7 @@ class UserController extends Controller
             "username" => "required|max:255|alpha_num|unique:users",
             "first_name" => "required|max:255|alpha",
             "last_name" => "required|max:255|alpha",
-            "date_of_birth" => "required|date|before:" . date("Y-m-d", Carbon::now()),
+            "date_of_birth" => "required|date|before:" . date("Y-m-d"),
             "phone_number" => "required|string|min:6|max:14|unique:users",
             "phone_code" => "required|exists:countries,id",
             "email" => "required|max:255|email|unique:users",
